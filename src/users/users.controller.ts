9d5452260a084222dd36e19f702236313f62fc8b
krb0 <maurodavidcaceres@gmail.com>
2022-05-29 19:45:17 -0300

    post handler added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private auth: UsersService) {}
+  @Post()
+  createUser(@Body() { email, password }: CreateUserDTO, @Res() res: Response) {
+    if (password.length <= 7) {
+      res.status(400).send('Password must be at least 8 characters long');
+    }
+    this.auth.create(email, password);
+    return 'user created';
+  }
+  @Get()
+  findAll(): string {
+    return 'This action returns all users';
+  }
+}
