835e9f99281516de1d10a34e0fdec81d979a125d
krb0 <maurodavidcaceres@gmail.com>
2022-05-29 20:01:24 -0300

    reports typeorm added

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateReportDTO } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+  @Post()
+  create(@Body() body: CreateReportDTO) {
+    this.reportsService.create(body);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Report } from './reports.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateReportDTO } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+  create(report: CreateReportDTO) {
+    return this.repo.save(report);
+  }
+}
