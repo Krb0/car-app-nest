3d95e606f83e71e593dca62e11008a4944bddd44
krb0 <maurodavidcaceres@gmail.com>
2022-06-02 22:05:18 -0300

    user remove added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Put,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -6,17 +15,36 @@ import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
   constructor(private auth: UsersService) {}
-  @Post()
+
+  @Put('/signup')
   createUser(@Body() { email, password }: CreateUserDTO, @Res() res: Response) {
     if (password.length <= 7) {
       res.status(400).send('Password must be at least 8 characters long');
       return;
     }
     this.auth.create(email, password);
-    return 'user created';
+    res.status(201).send('User created');
   }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.auth.findOne(parseInt(id));
+    return user ? user : 'User not found';
+  }
+
   @Get()
-  findAll(): string {
-    return 'This action returns all users';
+  async findAll(@Query('email') email: string) {
+    return await this.auth.find(email);
+  }
+
+  @Delete('remove/:id')
+  async removeUser(@Param('id') id: string, @Res() res: Response) {
+    res
+      .status(200)
+      .send(
+        (await this.auth.remove(parseInt(id)))
+          ? 'User removed'
+          : 'User not found',
+      );
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,4 +9,24 @@ export class UsersService {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+  async update(id: number, fields: Partial<User>) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) {
+      return null;
+    }
+    return this.repo.save({ ...user, ...fields });
+  }
+  async remove(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) {
+      return null;
+    }
+    return this.repo.remove(user);
+  }
 }
